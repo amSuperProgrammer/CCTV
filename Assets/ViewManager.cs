8f75241b43199f988e292aa04a3cb742703635e8
amSuperProgrammer <118238515+amSuperProgrammer@users.noreply.github.com>
2025-04-02 17:14:01 +0500

    123

--- Assets/ViewManager.cs
+++ Assets/ViewManager.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.Mathematics;
 
 public class ViewManager : MonoBehaviour
 {
     [SerializeField] List<ViewPoint> viewList = new List<ViewPoint>();
-    ViewPoint currentViewPoint;
+    public ViewPoint currentViewPoint;
 
     [SerializeField] float cameraSensivity = 60;
     [SerializeField] float fromToMoveSpeed = 5;
     [SerializeField] float fromToRotateSpeed = 5;
+
+    public float buttonHideAngle = 45;
 
     private void Start()
     {
@@ -20,21 +23,15 @@ public class ViewManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A)) 
         {
-            int viewPointIndex = viewList.IndexOf(currentViewPoint);
-            if (viewPointIndex == 0)
-                currentViewPoint = viewList[viewList.Count - 1];
-            else
-                currentViewPoint = viewList[viewPointIndex - 1];
+            PreviousViewPoint(); 
+            ReBuildViewPointOrder();
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            int viewPointIndex = viewList.IndexOf(currentViewPoint);
-            if (viewPointIndex == viewList.Count - 1)
-                currentViewPoint = viewList[0];
-            else
-                currentViewPoint = viewList[viewPointIndex + 1];
+            NextViewPoint(); 
+            ReBuildViewPointOrder();
         }
 
         if (transform.position != currentViewPoint.transform.position)
@@ -42,18 +39,71 @@ public class ViewManager : MonoBehaviour
             transform.position = Vector3.Lerp(transform.position, currentViewPoint.transform.position, Time.deltaTime * fromToMoveSpeed);
             transform.rotation = Quaternion.Lerp(transform.rotation, currentViewPoint.transform.rotation, Time.deltaTime * fromToRotateSpeed);
         }
-        else if (Input.GetMouseButton(0)) CameraRotate();
+        else if (Input.GetMouseButton(1))
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            mouseX = mouseX * Time.deltaTime * cameraSensivity;
+            float mouseY = -Input.GetAxis("Mouse Y");
+            mouseY = mouseY * Time.deltaTime * cameraSensivity;
+
+            if (currentViewPoint.lockHorizontalRotation)
+            {
+                float angle = Vector3.Angle(Camera.main.transform.forward, currentViewPoint.transform.forward);
+                Debug.Log($"Next angle: {angle - Mathf.Abs(mouseX)}");
+                // if (angle - Mathf.Abs(mouseX) < currentViewPoint.horizontalLockAngle)
+                    transform.Rotate(Vector3.up, mouseX, Space.World);
+            }
+            else
+                transform.Rotate(Vector3.up, mouseX, Space.World);
+
+            
+            if (currentViewPoint.lockVerticalRotation)
+            {
+                float angle = Vector3.Angle(Camera.main.transform.forward, currentViewPoint.transform.forward);
+                Debug.Log($"Next angle: {angle - Mathf.Abs(mouseY)}");
+                // if (angle - Mathf.Abs(mouseY) < currentViewPoint.verticalLockAngle)
+                    transform.Rotate(Vector3.right, mouseY);
+            }
+            else
+                transform.Rotate(Vector3.right, mouseY);
+        }
 
     }
 
-    private void CameraRotate()
+    public void PreviousViewPoint()
     {
-        float mouseX = Input.GetAxis("Mouse X");
-        mouseX = mouseX * Time.deltaTime * cameraSensivity;
-        float mouseY = -Input.GetAxis("Mouse Y");
-        mouseY = mouseY * Time.deltaTime * cameraSensivity;
+        int viewPointIndex = viewList.IndexOf(currentViewPoint);
+            if (viewPointIndex == 0)
+                currentViewPoint = viewList[viewList.Count - 1];
+            else
+                currentViewPoint = viewList[viewPointIndex - 1];
+    }
 
-        transform.Rotate(Vector3.up, mouseX, Space.World);
-        transform.Rotate(Vector3.right, mouseY);
+    public void NextViewPoint()
+    {
+        int viewPointIndex = viewList.IndexOf(currentViewPoint);
+        if (viewPointIndex == viewList.Count - 1)
+            currentViewPoint = viewList[0];
+        else
+            currentViewPoint = viewList[viewPointIndex + 1];
+    }
+
+    public void SetActiveViewPoint(ViewPoint viewPoint) => currentViewPoint = viewPoint;
+
+    public void ReBuildViewPointOrder()
+    {
+        Dictionary<float, ViewPoint> viewPointDictionary = new Dictionary<float, ViewPoint>();
+        List<float> distances = new List<float>();
+        foreach (ViewPoint viewPoint in viewList)
+        {
+            float distance = Vector3.Distance(viewPoint.transform.position, currentViewPoint.transform.position);
+            viewPointDictionary.Add(distance, viewPoint);
+            distances.Add(distance);
+        }
+        distances.Sort();
+        distances.Reverse();
+
+        foreach (float distance in distances)
+            viewPointDictionary[distance].UpUIHierarchy();
     }
 }
--- Assets/ViewPoint.cs
+++ Assets/ViewPoint.cs
@@ -3,29 +3,48 @@ using UnityEngine.UIElements;
 
 public class ViewPoint : MonoBehaviour
 {
+    [Tooltip("If true, the camera will not rotate horizontally when the mouse is moved left or right.")]
+    public bool lockHorizontalRotation = false;
+    public float horizontalLockAngle = 45f;
+    [Space(20)]
+    [Tooltip("If true, the camera will not rotate vertically when the mouse is moved up or down.")]
+    public bool lockVerticalRotation = false;
+    public float verticalLockAngle = 45f;
+
+    ViewManager _viewManager;
+
     VisualElement root;
     VisualElement cameraPoint;
+    Button pointButton;
     private void Start()
     {
+        _viewManager = Camera.main.GetComponent<ViewManager>();
+
         root = GameObject.Find("UIDocument").GetComponent<UIDocument>().rootVisualElement;
         VisualElement cameraPointTemplate = Resources.Load<VisualTreeAsset>("CameraPointTemplate").CloneTree();
-        cameraPointTemplate.name = $"button_{gameObject.name}";
+        cameraPointTemplate.name = $"point_{gameObject.name}";
         root.Add(cameraPointTemplate);
-        cameraPoint = cameraPointTemplate.Q<VisualElement>($"button_{gameObject.name}");
+        cameraPoint = cameraPointTemplate.Q<VisualElement>($"point_{gameObject.name}");
 
-        GameObject test = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        test.transform.position = transform.position;
-        test.transform.rotation = transform.rotation;
-        Debug.Log(Camera.main.WorldToScreenPoint(transform.position));
-
-        
+        pointButton = cameraPoint.Q<Button>();
+        pointButton.clicked += () => MakeThisViewPointAsActive();
     }
 
     private void LateUpdate()
     {
-        Vector3 pointInScreen = Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Vector3.Angle(Camera.main.transform.forward, transform.position - Camera.main.transform.position);
+        pointButton.style.opacity = angle < _viewManager.buttonHideAngle && _viewManager.currentViewPoint != this ? 1 : 0;
 
+        Vector2 pointInScreen = (Vector2)Camera.main.WorldToScreenPoint(transform.position);
         cameraPoint.style.left = pointInScreen.x;
         cameraPoint.style.top = Screen.height - pointInScreen.y;
     }
+
+    public void MakeThisViewPointAsActive()
+    {
+         _viewManager.currentViewPoint = this; 
+         _viewManager.ReBuildViewPointOrder();
+    }
+
+    public void UpUIHierarchy() => cameraPoint.BringToFront();
 }
